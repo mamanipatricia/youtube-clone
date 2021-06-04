1585469a11623417ac323066e0704aa8731b336e
patricia <patttymq@gmail.com>
2021-06-04 10:53:53 -0400

    create deploy script

---
+++ deploy-yt-frontent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# build
+# cd youtube-clone
+status=$(git status --porcelain)
+if [[ -z $status && -e .env.production ]];
+then
+yarn build
+cd build/
+echo pattty.com > CNAME
+surge
+else
+echo "cambios no guardados"
+git status
+exit 1;
+fi
