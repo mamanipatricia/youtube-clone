ecbf214cf0fc3be7b63789bbd009821be3a3e4b7
patricia <patttymq@gmail.com>
2021-06-22 09:31:46 -0400

    add subscription Comp design

--- src/components/Subscription/Subscription.js
+++ src/components/Subscription/Subscription.js
@@ -1,3 +1,26 @@
+import Icon from "../Icon/Icon";
+import styles from "./Subscription.module.css";
+
 export default function Subscription() {
-  return <div>SUBSCRIPTION</div>;
+  return (
+    <div className={styles.subscriptionContainer}>
+      <Icon
+        name="SUBSCRIPTION"
+        color="var(--bg-sentiment)"
+        className={{ width: "120", height: "120" }}
+      />
+      <div className={styles.textContent}>
+        <h1 className={styles.title}>Don&apos;t miss the new videos</h1>
+        <span>Login to see updates from your favorite YouTube channels</span>
+      </div>
+      <button className={styles.loginButton}>
+        <Icon
+          name="PROFILE"
+          color="var(--bg-type)"
+          className={{ width: "24", height: "24" }}
+        />
+        <span className={styles.singIn}>SIGN IN</span>
+      </button>
+    </div>
+  );
 }
