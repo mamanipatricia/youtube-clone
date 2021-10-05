5d52cbdc4aa49e7d486a165fb0f4a416d2482af1
patricia <patttymq@gmail.com>
2021-10-05 10:29:19 -0400

    change initial value

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ import styles from "./Home.module.css";
 import { MENU_HOME } from "../Constants/Constants";
 import { useAuth } from "../../context/authContext";
 
-const INITIAL_KEYWORD = "Terraform";
+const INITIAL_KEYWORD = "React";
 
 export default function Home() {
   const loading = useLoading();
