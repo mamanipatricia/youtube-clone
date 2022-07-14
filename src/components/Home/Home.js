dbf1e831de7c3d1c049cd2d9ff6c6eb3ab1401f3
patricia-w <patricia.mamani@coxautoinc.com>
2022-07-14 16:20:31 -0400

    change initial keyword

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ import styles from "./Home.module.css";
 import { MENU_HOME } from "../Constants/Constants";
 import { useAuth } from "../../context/authContext";
 
-const INITIAL_KEYWORD = "Reactjs";
+const INITIAL_KEYWORD = "aws";
 
 export default function Home() {
   const loading = useLoading();
